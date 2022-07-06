3ea70a612cfda5dacc451ae6da65a1eb2552a418
dev0623 <developer0623@gmail.com>
2022-07-07 00:22:02 +0900

    jakartanet

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,18 @@
 export default [
+    // {
+    //     explorerUrl: '',
+    //     nodeUrl: 'https://tezos-prod.cryptonomic-infra.tech:443',
+    //     url: 'https://conseil-prod.cryptonomic-infra.tech:443',
+    //     apiKey: 'galleon',
+    //     network: 'mainnet',
+    //     displayNetwork: 'mainnet',
+    // },
     {
         explorerUrl: '',
-        nodeUrl: '',
-        url: '',
-        apiKey: '',
-        network: 'mainnet',
-        displayNetwork: 'mainnet',
+        nodeUrl: 'https://tezos-jakarta.cryptonomic-infra.tech:443',
+        url: 'https://conseil-jakarta.cryptonomic-infra.tech:443',
+        apiKey: 'galleon',
+        network: 'jakartanet',
+        displayNetwork: 'jakartanet',
     },
 ];
