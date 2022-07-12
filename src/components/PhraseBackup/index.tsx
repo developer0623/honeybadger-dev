e014a2c37967a5eff4ca5ae437e7066a66908ae1
dev0623 <developer0623@gmail.com>
2022-07-12 19:19:59 +0100

    build iOS version

--- src/components/PhraseBackup/index.tsx
+++ src/components/PhraseBackup/index.tsx
@@ -145,7 +145,7 @@ const PhraseBackup = (props: any) => {
                         <Text style={styles.modalText}>Oops! Incorrect order</Text>
                         <Text style={styles.typo2}> Please enter your recovery phrase again to ensure you have written it down correctly .</Text>
                         <Button style={styles.modalBtn} onPress={generateNewPhrases}>
-                            <Text>Try Again</Text>
+                            <Text style={{color: 'white'}}>Try Again</Text>
                         </Button>
                     </View>
                 </View>
